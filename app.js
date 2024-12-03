44b8805c5e8eaef75a23a60efdc0cba0792e5bcb
imLost14 <hrdc.1407@hotmail.com>
2024-12-03 10:34:00 -0500

    un paso avanzado

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ import preguntas from "./backend/controllers/preguntas.js";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-import authRoutes from './backend/routes/auth_Routes.js'
+import authRoutes from './backend/routes/authRoutes.js'
+
+dotenv.config();
 
 const app = express();
 
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.urlencoded(
-  { extended: true}
-));
+// app.use(express.urlencoded(
+//   { extended: true}
+// ));
 app.use('simu/auth', authRoutes);
 
 const PORT = process.env.PORT || 3005;
