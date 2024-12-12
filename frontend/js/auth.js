92f15e7fb71652628d14506b63540fa2e5ab6a81
Jose Armando <josearmandodiazhernandez1@gmail.com>
2024-12-11 21:34:06 -0500

    Auth: Login y Register funcionando

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,26 +1,74 @@
+const Swal = window.Swal;
 const formLogin = document.getElementById('formLogin');
-// const formularioRegistro = document.getElementById('formRegistro'); TO-DO: REGISTER
+const formRegister = document.getElementById('formRegister'); 
 
-formLogin.addEventListener('submit',  async (event) => {
-    event.preventDefault();
+if (formLogin) {
+    formLogin.addEventListener('submit',  async (event) => {
+        event.preventDefault();
+        const correo = document.getElementById('email').value;
+        const contrasena = document.getElementById('password').value;
+    
+            const response = await fetch('http://localhost:3005/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ correo, contrasena }),
+            });
+    
+            const data = await response.json();
+            formLogin.reset();
+    
+            if (!response.ok) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Vaya...",
+                    text: `${data.mensaje}`,
+                    footer: 'Somos Simu'
+                  });
+                  return;
+            }
+            window.location.href = 'panel.html';
+    });
+}
 
-    const correo = document.getElementById('email').value;
-    const contrasena = document.getElementById('password').value;
-
-        const response = await fetch('http://localhost:3005/login', {
+if (formRegister) {
+    formRegister.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const nombre = document.getElementById('nombre').value;
+        const correo = document.getElementById('email').value;
+        const contrasena = document.getElementById('password').value;
+    
+        const response = await fetch('http://localhost:3005/registro', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ correo, contrasena }),
+            body: JSON.stringify({ nombre, correo, contrasena }),
         });
-
+    
         const data = await response.json();
-
+        formRegister.reset()
+    
         if (!response.ok) {
-            console.log(data.mensaje);
-            return;
+            Swal.fire({
+                icon: "error",
+                title: "Vaya...",
+                text: `${data.error[0].message}`,
+                footer: 'Somos Simu'
+              });
+              return;
         }
     
-        console.log('Token:', data.token ) // PRUEBA
-});
+        Swal.fire({
+            icon: "success",
+            title: `¡El registro fue éxitoso!`,
+            showConfirmButton: false,
+            timer: 1000
+          });
+          
+          setTimeout(() => {
+            window.location.href = 'login.html';
+          }, 1000);
+    });
+}
